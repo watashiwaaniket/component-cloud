958acce64f9c7d6d3abe3c4ab23454388cb0fc62
Aniket Dhakane <aniketdhakane3@gmail.com>
2024-10-14 00:55:56 +0530

    started working on the navbar component

---
+++ src/app/components/Navbar/page.js
@@ -0,0 +1,17 @@
+export default function page() {
+    return(
+        <>
+            <div className="navbar">
+                <div className="logo">
+                    <h1>LOGO</h1>
+                </div>
+                <div className="nav-buttons">
+                    <ul>
+                        <li><a href="/">+ Create</a></li>
+                        <li><a href="/">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+};
