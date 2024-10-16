531c20ec79de5da4f94fa3b0a1d45ee26516dca5
Aniket Dhakane <aniketdhakane3@gmail.com>
2024-10-16 20:24:41 +0530

    Landing page done, connected the dummy pages until auth

---
+++ src/app/components/Input.js
@@ -0,0 +1,10 @@
+export default function Input() {
+    return(
+        <section className="input-section">
+        <input placeholder="Search for components"></input>
+        <button>
+            Search
+        </button>
+        </section>
+    )
+};
---
+++ src/app/create-component/page.js
@@ -0,0 +1,9 @@
+export default function page() {
+    return(
+        <>
+            Here the create and submit page of the application Is
+            It should only be accessible when then user is logged in,
+            so we will need session handler and cookies 
+        </>
+    )
+};
