099146a41106240f316eba293c90c55966ffd094
Aniket Dhakane <aniketdhakane3@gmail.com>
2024-10-27 21:56:06 +0530

    set up the authentication for the application

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Navbar() {
+    const session = useSession();
     return(
         <>
             <div className="navbar">
@@ -10,7 +13,8 @@ export default function Navbar() {
                 <div className="nav-buttons">
                     <ul>
                         <li><Link href="/create-component">+ Create</Link></li>
-                        <li><Link href="/login">Login</Link></li>
+                        {session.data?.user && <li><button onClick={() => signOut()}>Logout</button></li>}
+                        {!session.data?.user && <li><button onClick={() => signIn()}>Login</button></li>}
                     </ul>
                 </div>
             </div>
