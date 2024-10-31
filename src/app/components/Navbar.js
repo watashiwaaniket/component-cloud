43b86f970bd2b300f6a359fcb6916e3a75152414
Aniket Dhakane <aniketdhakane3@gmail.com>
2024-10-31 18:38:26 +0530

    prisma init

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
-import Svg from "../assets/svg";
 
 
 export default function Navbar() {
